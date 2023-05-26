51cc6b06918986f2c1139f6b0982eb3c31245634
Alex <alexbevan20@gmail.com>
2023-05-26 13:53:11 +0100

    Initial commit - removed state at end

--- web.py
+++ web.py
@@ -25,7 +25,3 @@ for index, todo in enumerate(todos):
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key="new_todo")
-
-print("Hello")
-
-st.session_state
